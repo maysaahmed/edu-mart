9bb40a39cb81dc78b9e2dffb4fa227235d37a42d
Your Name <maysa.ahmed.iti@gmail.com>
2025-04-16 23:44:38 +0200

    add assessment count to organization list

--- Modules/Organizations/Transformers/OrganizationResource.php
+++ Modules/Organizations/Transformers/OrganizationResource.php
@@ -21,6 +21,7 @@ class OrganizationResource extends JsonResource
             'address' => $this->address ?? '-',
             'status' => $this->status,
             'domain' => $this->domain ?? '-',
+            'assessments' => $this->assessments()->count() ? $this->assessments()->count() . ' Assessments' : '-'
 
         ];
     }
